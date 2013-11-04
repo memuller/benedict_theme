eac5c99316ae5d6fdc304807019b2b9d4db5d5b9
Matheus E. Muller <hello@memuller.com>
2013-11-04 18:59:48 -0200

    Fixes search bar.

--- search-custom.php
+++ search-custom.php
@@ -8,12 +8,14 @@
  */
 ?>
 <div class="col span_1">
-	<a href="#" title="<?php esc_attr_e( 'Search', 'themelovin' ); ?>" id="iconFadeIn"><span class="pictogram">&#128269;</span></a>
+	<a href="#" title="<?php esc_attr_e( 'Search', 'themelovin' ); ?>" id="iconFadeIn"><span class="search" style="width: 22px; height: 22px; vertical-align: middle;">
+		<img src="<?php echo get_template_directory_uri() ?>/images/header-search-icon.png" alt="">
+	</span></a>
 </div>
 <div id="overlay"></div>
 <div id="searchFadeIn">
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="text" class="field" name="s" id="s" data-placeholder="<?php esc_attr_e( 'What are you looking for?', 'themelovin' ); ?>" value="<?php esc_attr_e( 'What are you looking for?', 'themelovin' ); ?>" autocomplete="off" />
+		<input type="text" class="field" name="s" id="s" data-placeholder="<?php echo('O que está procurando?') ?>" value="<?php echo('O que está procurando?') ?>" autocomplete="off" />
 		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'themelovin' ); ?>" />
 	</form>
 </div>
