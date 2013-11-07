b782ea122667b39370336bd5aece1e9386e11d26
Matheus E. Muller <hello@memuller.com>
2013-11-06 23:04:10 -0200

    Implements FB comments.

--- comments.php
+++ comments.php
@@ -1,32 +1,9 @@
-<?php
-/**
- * The Template for display comments.
- *
- * @package WordPress
- * @subpackage Bloq
- * @since Bloq 1.0
- */
-?>
-<div id="comments">
-	<h1><?php printf( _n('%1$s Comment', '%1$s Comments', get_comments_number(), 'themelovin'), number_format_i18n(get_comments_number())); ?></h1>
-	<?php if(post_password_required()) { ?>
-		<p class="nopassword"><?php _e('This post is password protected. Enter the password to view any comments.', 'themelovin'); ?></p>
+<div class="meta box horizontal sharing">
+	<span class="share meta">
+		<?php inline_svg('/images/icons/person.svg', 'icon')  ?>
+		<span>Compartilhe</span>
+	</span>
+	<?php echo sharing_display(); ?>
 </div>
-<?php
-	return;
-	}
-if(have_comments()) {
-?>
-<ol class="commentlist">
-		<?php wp_list_comments(array( 'callback' => 'glg_comment')); ?>
-	</ol>
-<?php
-	}
-	if (get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
-?>
-	<nav class="comment-navigation">
-    	<?php paginate_comments_links(); ?>
-    </nav>
-<?php endif; ?>
-</div>	
-<?php comment_form(glg_edit_comment_form()); ?>
+<div class="fb-comments" data-href="<?php the_permalink() ?>" data-num-posts="4" data-width="700" data-colorscheme="light" data-mobile="false">
+</div> 
